5d602039519c230e7fd32fb5bafbf3a7646ea1ad
Jer Carlo Catallo <jcdcatallo@rpinnotech.com>
2020-11-26 07:53:54 +0800

    deployment

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const monggose = require("mongoose");
 const bodyParser = require("body-parser");
 const items = require("./routes/api/items");
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = "mongodb://localhost:27017/";
-const DATABASE_NAME = "jer_shopping_list_db";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/";
+const DATABASE_NAME = process.env.DATABASE || "jer_shopping_list_db";
 
 monggose
   .connect(`${MONGO_URL}${DATABASE_NAME}`, {
@@ -21,5 +22,15 @@ monggose
   .catch((err) => console.log(err));
 
 app.use("/api/items", items);
+
+//Serve static assets in in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening to PORT: ${PORT}`));
